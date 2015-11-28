e80a8c4e5603cba71aa21030dde531a9851a571e
jujudorange <juliette33f@orange.fr>
2015-11-28 16:13:46 +0100

    My first blog

---
+++ exercices/python_intro.py
@@ -0,0 +1,11 @@
+nombre = 50
+if nombre < 5:
+	print("C'est trop peu")
+elif 15 <= nombre < 25:
+	print("C'est mieux mais il faudrait plus")
+elif 25 <= nombre < 40:
+	print("On y est presque!")
+elif 40 <= nombre < 60
+	print("Par-fait.")
+elif 60 <= nombre < 80
+	print("Euuh un peu trop quand même...")
